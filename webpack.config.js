ac94598f4a8940d52710583b990fbd5acfb140fa
svRed <derevyankosvitlana2015@gmail.com>
2017-09-03 15:43:49 +0300

    Create config file

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry:  __dirname + '/src/index.js',
+    output: {
+        path: __dirname+'/build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    devtool: 'source-map',
+    plugins: [HtmlWebpackPluginConfig]
+};
